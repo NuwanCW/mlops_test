b215c38d5869f9196e9f18301349bbee3c251b09
Nuwan Chamila Withana <wgnuwanc@gmail.com>
2022-03-09 11:55:43 +1100

    reset

--- docs/tagifai/data.md
+++
@@ -1,3 +0,0 @@
-
-
-::: tagifai.data
--- tests/config/test_config.py
+++
@@ -1,8 +0,0 @@
-# tests/config/test_config.py
-# Test config/config.py components.
-
-from config import config
-
-
-def test_config():
-    assert config.logger.name == "root"
